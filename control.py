46cbd82eecffeff3efa6f3c60b9497914afbab23
Cobus Smit <cobus@smitsa.co.za>
2024-07-24 16:06:49 +0200

    Implement on off controls

--- control.py
+++ control.py
@@ -1,5 +1,16 @@
 from nicegui import ui
 import seeed_dht
+import grovepi
+import time
+
+onswitchPin = 5
+offswitchPin = 16
+
+grovepi.pinMode(onswitchPin,"OUTPUT")
+grovepi.pinMode(offswitchPin,"OUTPUT")
+
+grovepi.digitalWrite(onswitchPin,0)
+grovepi.digitalWrite(offswitchPin,0)
 
 sensor = seeed_dht.DHT("11", 12)
 
@@ -7,11 +18,24 @@ ui.label('Bathroom Extractor Fan Monitor')
 with ui.row():
     tempLabel = ui.label('Temp: ?')
     humidityLabel = ui.label('Humidity: ?')
+with ui.row():
+    ui.button('Fan On', on_click=lambda: switchFanOn())
+    ui.button('Fan Off', on_click=lambda: switchFanOff())
 
 def refresh():
     humi, temp = sensor.read()
     tempLabel.set_text(f'Temp: {temp}')
     humidityLabel.set_text(f'Humidity: {humi}')
+
+ref switchFanOn():
+    grovepi.digitalWrite(onswitchPin,1)
+    time.sleep(1);
+    grovepi.digitalWrite(onswitchPin,0)
+
+ref switchFanOff():
+    grovepi.digitalWrite(offswitchPin,1)
+    time.sleep(1);
+    grovepi.digitalWrite(offswitchPin,0)
 
 refresh()
 ui.timer(5.0, lambda: refresh())
